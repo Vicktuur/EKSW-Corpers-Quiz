8631f37a051ca00b160e14103975ec7ea2e0c769
Abiye Iniabere <104025786+Vicktuur@users.noreply.github.com>
2023-03-05 23:05:43 +0100

    first commit

---
+++ js/questions.js
@@ -0,0 +1,109 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who was the CLO that just passed out from A2 2022 set?",
+    answer: "Momosani Sadiq",
+    options: [
+      "Mrs Olubodun Kikelomo",
+      "Momosani Sadiq",
+      "Eniola Kolapo",
+      "Edward Cynthia"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is the (LGI 2) of Ekiti South West?",
+    answer: "Mr Omotade O.B",
+    options: [
+      "Mr Kelvin Luper",
+      "Mrs Olobodun Kikelomo",
+      "Mr Omotade O.B",
+      "Mr Alonge O."
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who was the General C.D.S coordinator of Ekiti South West that just passed out from A2 2022 set?",
+    answer: "Eniola kolapo",
+    options: [
+      "Sunday Pius",
+      "Nweke Scholastica",
+      "Momosani Sadiq",
+      "Eniola kolapo"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the LGI of Ekiti South West?",
+    answer: "Mrs Olobodun Kikelomo",
+    options: [
+      "Mr Kelvin Luper",
+      "Mr Alonge O.",
+      "Mrs Olobodun Kikelomo",
+      "Mr Omotade O.B"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What day does the monthly biometric clearance take place?",
+    answer: "First Thurday of every month",
+    options: [
+      "Last Friday of every month",
+      "First Thurday of every month",
+      "First Igbara-odo market day of every month",
+      "Anyday the LGI feels like calling us"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the full meaning of C.D.S?",
+    answer: "Community Development Service",
+    options: [
+      "Community Development Scheme",
+      "Compound Development Scheme",
+      "Community Development Service",
+      "Community Deterioration Scheme"
+    ]
+  }, {
+    numb: 7,
+    question: "How many Batches of Corpers does the NYSC Scheme inaugurate in a year?",
+    answer: "3",
+    options: [
+      "5",
+      "2",
+      "3",
+      "1"
+    ]
+  }, {
+    numb: 8,
+    question: "Who is the current D.G of NYSC?",
+    answer: "Brig. Gen. Yushau Dogara",
+    options: [
+      "Brig. Gen. Shuaibu Ibrahim",
+      "Gen. M.K Fadah",
+      "Brig. Gen. Yushau Dogara",
+      "Gen. Chikezie Mary Nnenna"
+    ]
+  }, {
+    numb: 9,
+    question: "Who is the State cordinator of NYSC in Ekiti State?",
+    answer: "Mrs Chikezie Mary Nnenna",
+    options: [
+      "Mr Iniabere Victor",
+      "Mrs Chikezie Mary Nnenna",
+      "Major Abel Agbo",
+      "Mr Tosin Afolabi"
+    ]
+  }, {
+    numb: 10,
+    question: "What is the duration of the NYSC scheme?",
+    answer: "1 year",
+    options: [
+      "6 months",
+      "2 years",
+      "8 months",
+      "1 year"
+    ]
+  },
+];
